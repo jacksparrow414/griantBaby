c79c27af99d69c57b852690c5a9d1bf1b9f2b942
jacksparrow414 <33389378+jacksparrow414@users.noreply.github.com>
2020-03-14 21:22:23 +0800

    feat:前端已经打好的包、nginx配置文件

--- code/sql/sys_user.sql
+++ code/sql/sys_user.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : MySQL
+ Source Server         : mysql3306
  Source Server Type    : MySQL
- Source Server Version : 50724
+ Source Server Version : 80018
  Source Host           : localhost:3306
  Source Schema         : baby
 
  Target Server Type    : MySQL
- Target Server Version : 50724
+ Target Server Version : 80018
  File Encoding         : 65001
 
- Date: 07/09/2019 14:05:23
+ Date: 14/03/2020 21:15:38
 */
 
 SET NAMES utf8mb4;
@@ -23,13 +23,19 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `sys_user`;
 CREATE TABLE `sys_user` (
   `id` bigint(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `user_name` varchar(10) DEFAULT NULL COMMENT '名字',
-  `age` varchar(32) DEFAULT NULL COMMENT '机构ID',
-  `email` varchar(32) DEFAULT NULL COMMENT '登录邮箱',
-  `password` varchar(100) DEFAULT NULL COMMENT '登录密码',
-  `test` tinyint(3) unsigned DEFAULT NULL,
-  `salt` varchar(20) DEFAULT NULL COMMENT '盐',
+  `user_name` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '名字',
+  `age` int(32) NOT NULL COMMENT '机构ID',
+  `email` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '登录邮箱',
+  `password` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '登录密码',
+  `salt` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '盐',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1238704634990817283 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sys_user
+-- ----------------------------
+BEGIN;
+INSERT INTO `sys_user` VALUES (1238704634990817282, 'admin', 24, '2990151812@qq.com', 'c05f2abfabebc47c4ecf3476444e28d2ef57c65f0657c1519412f08235c3aa7a', 'QUOheNLeASTTIgpjzvdo');
+COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
